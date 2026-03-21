0f0db9c65d41fe0e4d15fb894ac4272989a4abbd
yurin <hoshinana.eight.nine.ten.y@gmail.com>
2026-03-21 17:36:52 +0900

    詳細ページのレイアウト変更　　

--- src/resources/views/admin/show.blade.php
+++ src/resources/views/admin/show.blade.php
@@ -12,79 +12,82 @@
             <h2>詳細</h2>
         </div>
     </div>
-    <div class="confirm-table">
-        <table class="confirm-table__inner">
-            <tr class="confirm-table__row">
-                <th class="confirm-table__header">お名前</th>
-                <td class="confirm-table__text">
-                    <p>{{ $contact->last_name }}　{{ $contact->first_name }}
-                    </p>
-                </td>
-            </tr>
-            <tr class="confirm-table__row">
-                <th class="confirm-table__header">性別</th>
-                <td class="confirm-table__text">
-                    <p>@if($contact->gender == 1) 男性
-                        @elseif($contact->gender == 2) 女性
-                        @else その他 @endif
-                    </p>
-                </td>
-            </tr>
-            <tr class="confirm-table__row">
-                <th class="confirm-table__header">メールアドレス</th>
-                <td class="confirm-table__text">
-                    <p>{{ $contact->email }}</p>
-                </td>
-            </tr>
-            <tr class="confirm-table__row">
-                <th class="confirm-table__header">電話番号</th>
-                <td class="confirm-table__text">
-                    <p>{{ $contact->tel }}</p>
-                </td>
-            </tr>
-            <tr class="confirm-table__row">
-                <th class="confirm-table__header">住所</th>
-                <td class="confirm-table__text">
-                    <p>{{ $contact->address }}</p>
-                </td>
-            </tr>
-            <tr class="confirm-table__row">
-                <th class="confirm-table__header">建物名</th>
-                <td class="confirm-table__text">
-                    <p>{{ $contact->building }}</p>
-                </td>
-            </tr>
-            <tr class="confirm-table__row">
-                <th class="confirm-table__header">お問い合わせの種類</th>
-                <td class="confirm-table__text">
-                    {{ optional($contact->category)->content ?? '未設定' }}
-                </td>
-            </tr>
-            <tr class="confirm-table__row">
-                <th class="confirm-table__header">お問い合わせの種類</th>
-                <td class="confirm-table__text">
-                    <p>{{ $contact->detail }}</p>
-                </td>
-            </tr>
-            <tr class="confirm-table__row">
-                <th class="confirm-table__header">どこで知りましたか？</th>
-                <td class="confirm-table__text">
-                    @foreach($channels as $channel)
-                    <p>{{ $channel->content }}</p>
-                    @if(!$loop->last), @endif @endforeach
-                </td>
-            </tr>
-            <tr class="confirm-table__row">
-                <th class="confirm-table__header">画像ファイル</th>
-                <td class="confirm-table__text">
-                    <p>@if($contact->image_file)
-                        <img src="{{ asset('storage/' . $contact->image_file) }}">
-                        @else
-                    <p>画像はアップロードされていません</p>
-                    @endif
-                    </p>
-                </td>
-            </tr>
-
+    <div class="form">
+        <div class="confirm-table">
+            <table class="confirm-table__inner">
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">お名前</th>
+                    <td class="confirm-table__text">
+                        <p>{{ $contact->last_name }}　{{ $contact->first_name }}
+                        </p>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">性別</th>
+                    <td class="confirm-table__text">
+                        <p>@if($contact->gender == 1) 男性
+                            @elseif($contact->gender == 2) 女性
+                            @else その他 @endif
+                        </p>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">メールアドレス</th>
+                    <td class="confirm-table__text">
+                        <p>{{ $contact->email }}</p>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">電話番号</th>
+                    <td class="confirm-table__text">
+                        <p>{{ $contact->tel }}</p>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">住所</th>
+                    <td class="confirm-table__text">
+                        <p>{{ $contact->address }}</p>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">建物名</th>
+                    <td class="confirm-table__text">
+                        <p>{{ $contact->building }}</p>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">お問い合わせの種類</th>
+                    <td class="confirm-table__text">
+                        {{ optional($contact->category)->content ?? '未設定' }}
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">お問い合わせの種類</th>
+                    <td class="confirm-table__text">
+                        <p>{{ $contact->detail }}</p>
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">どこで知りましたか？</th>
+                    <td class="confirm-table__text">
+                        @foreach($channels as $channel)
+                        <p>{{ $channel->content }}</p>
+                        @if(!$loop->last), @endif @endforeach
+                    </td>
+                </tr>
+                <tr class="confirm-table__row">
+                    <th class="confirm-table__header">画像ファイル</th>
+                    <td class="confirm-table__text">
+                        <p>@if($contact->image_file)
+                            <img src="{{ asset('storage/' . $contact->image_file) }}" class="confirm-table__image">
+                            @else
+                        <p>画像はアップロードされていません</p>
+                        @endif
+                        </p>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </main>
 @endsection
